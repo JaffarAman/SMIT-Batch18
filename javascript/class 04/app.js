d9db60028d73914a261fc67123534fa1b3f755f5
JaffarAman <amanjaffar50@gmail.com>
2025-12-15 22:50:47 +0500

    if elese

---
+++ javascript/class 04/app.js
@@ -0,0 +1,139 @@
+// var userName;
+
+
+// userName  = "Jaffar Aman"
+
+// console.log(userName)
+
+
+
+// var num1 = 10
+
+// // num1 = num1 + 1
+
+
+// // console.log(num1++) //post
+// console.log(++num1) //pre
+
+
+
+// var a = 12 // 13 /12 /11 / 12 / 13
+// var res = a + a++ - --a - a-- + a++ + ++a - a
+//     //   12 + 12  - 12   - 12 + 11 +  13  - 13 
+//     //     11   
+
+// console.log(res)
+
+
+
+
+
+// alert("Enter your name")
+// var userName = "Jaffar Aman"
+
+// var userName = prompt("Enter your name")
+// console.log(userName)
+
+// alert("welcome" + " " + userName)
+
+
+// document.writeln("<h1>Hello world 1</h1> <br />")
+// document.writeln("Hello world 2 <br /> ")
+// document.writeln("Hello world 3 <br /> ")
+
+
+
+// var user = prompt("Enter your name" , "Jaffar aman" )
+
+// document.writeln("welcome: " + user)
+
+
+// var name1 = prompt("Enter your name");
+// var age = prompt("Enter your age");
+// var email = prompt("Enter your email")
+// var gender = prompt("Enter your gender")
+
+// // <li>Name: Jaffar </li>
+// document.writeln("<ul>")
+// document.writeln("<li> Name: " + name1 + "</li>")
+// document.writeln("<li>Age: " + age + "</li>")
+// document.writeln("<li>Email: " + email + "</li>")
+// document.writeln("<li>Gender: " + gender + "</li>")
+// document.writeln("</ul>")
+
+
+// console.log(name)
+// console.log(age)
+// console.log(email)
+// console.log(gender)
+
+
+
+
+// var num1 = +prompt("Enter num1")
+// var num2 = +prompt("Enter num2")
+
+// var res = num1 + num2
+
+// document.writeln("Result: " + res)
+
+
+// var a = 100
+// console.log(typeof(a));
+
+
+
+// if statement
+
+
+// if( condition ){
+
+// }else{
+
+// }
+
+// var age = 28
+
+// if (age == 18) {
+//     console.log("allow")
+// } else {
+//     console.log("not allow")
+// }
+
+
+
+// var age = "20"
+
+// if (age === "20") {
+//     console.log("allow")
+// } else {
+//     console.log("not allow")
+// }
+
+
+// var userAge = +prompt('Enter your Age...');
+
+// if (userAge === 18) {
+//     console.log("allow")
+// } else {
+//     console.log("not allow")
+// }
+
+
+var age = 19
+
+if (age <= 18) {
+    console.log("allow")
+} else {
+    console.log("not allow")
+
+}
+
+
+
+// if (age >= 18) {
+//     console.log("allow")
+// } else {
+//     console.log("not allow")
+
+// }
