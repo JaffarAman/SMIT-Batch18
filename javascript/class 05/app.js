68448ee790744e66c3d05d036724971d86803201
JaffarAman <amanjaffar50@gmail.com>
2025-12-19 22:43:31 +0500

    if condition

---
+++ javascript/class 05/app.js
@@ -0,0 +1,104 @@
+// var email = prompt("enter email sigup ")
+// var password = prompt("enter password sigup")
+
+// var loginEmail = prompt("enter  Login email")
+// var loginPassword = prompt("enter Login password")
+
+// console.log(email, password)
+
+// if(email === loginEmail || password === loginPassword){
+//     alert("You are login..")
+// }else{
+//     alert("invalid user")
+// }
+
+
+
+
+// Gates => AND && Gate or || OR 
+
+
+
+// // Signup Data's
+// var userEmail = "admin@gmail.com"
+// var userPass = "admin@123"
+// // ============= //
+
+
+
+// var userEmailInput = prompt("Enter your email address")
+// var userPassInput = prompt("Enter your password address")
+
+// console.log(userEmailInput);
+// if (userEmailInput === userEmail && userPass === userPassInput) {
+//     console.log("login.......")
+// } else {
+//     console.log("invalid user")
+// }
+
+
+// var age = 10
+// var gender = "male"
+//         //false ||  true
+// if (age >= 18 ||  gender == "male") {
+//     console.log("allow")
+// } else {
+//     console.log("not allow")
+// }
+
+
+// =
+// ==
+// ===
+// > <  >=  <=
+// Gates && ||
+
+
+
+// male  => head office , ZA
+// female  => phase 6 , zamzama
+// other => not found
+// var gender = prompt("Enter your gender")
+
+// if (gender === "male") {
+//     console.log("head office")
+// } else if (gender == "female") {
+//     console.log("phase 6")
+// } else {
+//     console.log("not found")
+// }
+
+
+
+
+
+// // static
+// var num1 = 20
+// var num2 = 50
+// var opt = "-"
+
+// dynamic
+// var num1 = +prompt("enter num1")
+// var num2 = +prompt("enter num2")
+// var opt = prompt("enter operator")
+
+
+// if (opt == "+") {
+//     console.log("Add", num1 + num2)
+// } else if (opt == "-") {
+//     console.log("Sub", num1 - num2)
+// } else if (opt === "*") {
+//     console.log("Mul", num1 * num2)
+// }
+// else if (opt === "/") {
+//     console.log("Div", num1 / num2)
+// } else {
+//     console.log("invald operator!")
+// }
+
+
+
+
+p1
+p5
+percentage = (obt / total) * 100
