471f0c3abe9565916bb9bbf9a0d76ace5340a81e
JaffarAman <amanjaffar50@gmail.com>
2025-12-26 22:45:25 +0500

    array method

---
+++ javascript/class 07/app.js
@@ -0,0 +1,136 @@
+// Array
+// collection of data
+
+// var fruit1 = "apple"
+// var fruit2 = "orange"
+// var fruit3 = "mango"
+
+// var city1 = "karachi"
+// var city2 = "lahore"
+// var city3 = "multan"
+
+//              0           1           2             3
+// var cities = ["karachi", "lahore" , "islamabad" , "multan"]
+// console.log(cities[indexNumber])
+// console.log(cities)
+// console.log(cities[0])
+// console.log(cities[1])
+// console.log(cities[3])
+// console.log(cities[2])
+
+// console.log(cities[1])
+
+
+
+// var userName;
+
+// userName = "Jaffar"
+
+// console.log(userName);
+
+
+
+
+// var arr = []
+
+
+// arr[0] = "Jaffar"
+// arr[1] = "Aman"
+// arr[2] = "Pakistan"
+// arr[4] = "karachi"
+
+// console.log(arr[3]);
+
+
+
+
+
+// var arr = ["karachi" , "lahore" , "multan"]
+
+
+
+// arr[1] = "Islamabad"
+
+
+// console.log(arr);
+// console.log(arr.length);
+
+
+
+// Array methods ()
+// pop , push => last index
+// shift ,unshift => starting index
+// slice , splice
+
+// var cities = ["karachi", "lahore", "multan", "Islamabad"]
+
+
+// remove krta hai kaha sy last sy
+// cities.pop()
+
+// add value on last index
+// cities.push("Pakistan", "Srilanka" , "UK" , "USA")
+
+// remove krta hai kaha sy starting sy
+// cities.shift()
+// cities.shift()
+// 
+// cities.unshift("pakistan" )
+
+
+
+// console.log(cities)
+
+
+
+
+
+// slice
+
+// arr.slice(starting point , ending Point)
+// arr.slice(indexNumber , length)
+// arr.slice(1 , 3)
+
+
+// var abcd = arr.slice(1, 3)
+// console.log("abcd", abcd)
+// console.log("arr", arr)
+
+
+
+// var arr = ["karachi", "lahore", "multan", "islamabad"]
+
+// var newArr = arr.slice(1, 4)
+// var newArr = arr.slice(1)
+// var newArr = arr.slice(-2)
+// var newArr = arr.slice(1 , -1)
+
+// console.log(arr, "arr");
+// console.log(newArr, "newArr");
+
+
+
+var arr = ["karachi", "lahore", "multan", "islamabad"]
+
+// slice
+
+// var newArr = arr.slice(starting  ,ending )
+// var newArr = arr.slice(index  ,length )
+// var newArr = arr.slice(0, 3) //"karachi", "lahore", "multan"
+// var newArr = arr.slice(1 , 2)  //get only lahore
+// var newArr = arr.slice(1)  //get only lahore
+// var newArr = arr.slice(-3)  //get only lahore
+var newArr = arr.slice(0, 3)  //get only lahore
+
+
+console.log(newArr);
+
+
+
+
+
+
+
+
+
+
