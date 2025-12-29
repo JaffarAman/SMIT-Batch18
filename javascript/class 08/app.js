918fc28dd7e4e9e207beade45bb355af924fcf70
JaffarAman <amanjaffar50@gmail.com>
2025-12-29 22:52:32 +0500

    for loop

---
+++ javascript/class 08/app.js
@@ -0,0 +1,130 @@
+
+
+// arr.pop()
+// arr.push("multan")
+
+
+// arr.shift()
+// arr.unshift("multan")
+
+// var arr = ["karachi", "lahore", "multan", "apple", "islamabad", "burger"]
+
+
+// slice(starting point , ending point)
+// slice(index number , length number)
+// var arrCopy = arr.slice(1, 4)
+// var arrCopy = arr.slice(1)
+// var arrCopy = arr.slice(-1) //last value
+// var arrCopy = arr.slice(-3) //last 3 values
+// var arrCopy = arr.slice(0 , -2)
+
+
+
+
+
+// console.log("arr", arr)
+// console.log("arrCopy", arrCopy)
+
+
+
+// var arr = ["karachi", "lahore", "multan", "apple", "islamabad", "burger"]
+
+// // splice => delete or Add dono kam krta hai
+// // splice(starting point , delete Count , values  )
+// // splice(index number , 2  )
+
+// arr.splice(3, 1, "india")
+
+// console.log(
+//     arr
+// )
+
+
+// var arr = ["karachi", "lahore", "multan", "apple", "islamabad", "burger"]
+
+
+// var arr = ["karachi", "headoffice", "saylani"]
+// console.log(arr)
+
+
+
+
+
+
+// document.writeln(1)
+
+// for loop
+
+// for(initialization; condition; increment/decrement){
+
+// }
+
+// for (var a = 1; a <= 5; a++) {
+//     console.log("Jaffar")
+// }
+
+
+// step1=> initialization
+// step2=> check condition
+// step3 => if condition true > run block of code
+// step4 => increment/decrement
+// step5 => again check condition
+// step6 => repeat
+// if condition false = loop khatam tata bye bye Allah Hafiz
+
+
+
+
+// for (var a = 1; a <= 5; a++) {
+//     console.log(a)
+// }
+
+
+
+// for (var i = 10; i <= 100; i = i + 5) {
+//     document.writeln(i + "<br />")
+// }
+
+
+// for(var a = 1 ; a<=10 ; a++){
+//     document.writeln("2 x " + a + " =" + 2*a + "<br />")
+// }
+
+
+// document.writeln("2 x 1 = 2" + "<br />")
+// document.writeln("2 x 2 = 4" + "<br />")
+// document.writeln("2 x 3 = 6" + "<br />")
+// document.writeln("2 x 4 = 8" + "<br />")
+
+
+// for (var i = 8; i <= 20; i++) {
+//     document.writeln("13 x " + i + " =" + 13 * i + "<br />");
+// }
+
+
+// var tableNumber = +prompt("Enter Table Number")
+// var startNumber = +prompt("Enter Table start Number")
+// var endNumber = +prompt("Enter Table end Number")
+
+// for (var i = startNumber; i <= endNumber; i++) {
+
+//     document.writeln(tableNumber + "x" + i + "=" + tableNumber * i + "<br />")
+
+// }
+
+
+
+var arr = ["karachi", "lahore", "multan", "apple", "islamabad", "burger",]
+
+
+for (var i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+
+}
+
+
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+// console.log(arr[4])
+// console.log(arr[5])
